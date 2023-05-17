603236d706f3884f52bb87a7df0f64b6b1cada80
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-05-17 15:26:15 +0200

    NavLink

--- src/components/NavLinks/NavLinks.jsx
+++ src/components/NavLinks/NavLinks.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom'
+
+
 function NavLinks() {
   return (
     <ul>
-      <li>Movies</li>
-      <li>About</li>
-      <li>Contact</li>
+      <li><Link to="/movies">Movies</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
     </ul>
   )
 }

fcb0237cc308b2bcdd8d11d4cbf58937f620d678
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-03-24 11:13:22 +0100

    Filter movies

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,36 +1,52 @@
-import FilterDropDown from '../FilterDropDown/FilterDropDown';
-import MovieCard from '../MovieCard/MovieCard';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import FilterDropDown from "../FilterDropDown/FilterDropDown";
+import MovieCard from "../MovieCard/MovieCard";
+import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import Col from "react-bootstrap/Col";
 import "./MovieList.css";
 
 function MovieList() {
   const [movies, setMovies] = useState([]);
 
+  const [filterParams, setFilterParams] = useSearchParams({
+    filter: "popular",
+  });
+
   useEffect(() => {
-    const fetchMovies = async () => {
-      const res = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${
-          import.meta.env.VITE_API_KEY
-      }`
-      );
-      const data = await res.json();
-      
-      setMovies(data.results);
-    };
-    fetchMovies();
-  }, []);
+    fetch(
+      `https://api.themoviedb.org/3/movie/${filterParams.get(
+        "filter"
+      )}?api_key=${import.meta.env.VITE_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => setMovies(data.results));
+  }, [filterParams]);
 
   return (
-    
     <div className="movie_list">
       <div className="movie_list_filter">
-        <h4>Filter movies</h4>
-        <FilterDropDown />
+        <Col
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "0.5rem",
+          }}
+        >
+          <i className="fa-solid fa-filter"></i> Filter
+        </Col>
+        <FilterDropDown
+          filter={filterParams.get("filter")}
+          setFilter={setFilterParams}
+        />
       </div>
-      <h2>Popular movies</h2>
+      <h2>
+        {filterParams.get("filter") != "top_rated"
+          ? filterParams.get("filter") + " Movies"
+          : "Top Rated Movies"}
+      </h2>
       <div className="movie_grid">
-      {movies.map((movie) => (
+        {movies.map((movie) => (
           <Link to={`${movie.id}`} key={movie.id}>
             <MovieCard movie={movie} />
           </Link>

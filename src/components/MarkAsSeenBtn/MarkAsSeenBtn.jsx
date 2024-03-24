23788b95a017be7c8fe999312fa145f919a67e1a
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-03-24 09:21:14 +0100

    Add button from react-bootstrap and add css style

--- src/components/MarkAsSeenBtn/MarkAsSeenBtn.jsx
+++ src/components/MarkAsSeenBtn/MarkAsSeenBtn.jsx
@@ -1,7 +1,8 @@
-function MarkAsSeenBtn () {
-  return (
-    <button>Seen it</button>
-  )
+import Button from 'react-bootstrap/Button';
+
+function MarkAsSeenBtn() {
+  return <Button style={{backgroundImage: 'linear-gradient(to bottom right, #FDC639, #FF7C60', border: 0, cursor: 'pointer'}}
+  >Seen it</Button>;
 }
 
-export default MarkAsSeenBtn
+export default MarkAsSeenBtn;

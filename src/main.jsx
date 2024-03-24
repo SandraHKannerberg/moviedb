9ba0d9ca43d1743bf853cdfdb79f05d05ffa3129
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-03-24 21:46:36 +0100

    Add MovieProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { MovieProvider } from "../src/context/MovieContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <MovieProvider>
+      <App />
+    </MovieProvider>
   </BrowserRouter>
 );

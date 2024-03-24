9c4638a0ed5dfa79df5994403af8b24af17a3f18
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-03-24 21:46:13 +0100

    Function addToSeenMovies is working with button click

--- src/components/MarkAsSeenBtn/MarkAsSeenBtn.jsx
+++ src/components/MarkAsSeenBtn/MarkAsSeenBtn.jsx
@@ -1,59 +1,28 @@
 import Button from "react-bootstrap/Button";
-import { useState, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
+import { MovieContext } from "../../context/MovieContext";
 
 function MarkAsSeenBtn({ movie }) {
-  const initialMovies = JSON.parse(localStorage.getItem("myMovies")) || [];
-  const initialIsMovieSeen =
-    JSON.parse(localStorage.getItem(`isMovieSeen_${movie.id}`)) || false;
-
-  // My movielist
-  const [myMovies, setMyMovies] = useState(initialMovies);
-
-  // Boolean if a movie is mark as seen
-  const [isMovieSeen, setIsMovieSeen] = useState(initialIsMovieSeen);
-
-  const markMovieAsSeen = () => {
-    const currentMovie = {
-      image: movie.poster_path,
-      title: movie.title,
-      id: movie.id,
-    };
-
-    // Check if currentMovie already exists in myMovies
-    if (!myMovies.some((movie) => movie.id === currentMovie.id)) {
-      // If no - add movie
-      setMyMovies([...myMovies, currentMovie]);
-      setIsMovieSeen(true);
-      localStorage.setItem(
-        "isMovieSeen_" + currentMovie.id,
-        JSON.stringify(true)
-      );
-
-      const updateMyMoviesEvent = new CustomEvent("updateMyMovies");
-      window.dispatchEvent(updateMyMoviesEvent);
-    } else {
-      // If yes - don't add it again
-      console.log(`${currentMovie.title} finns redan i listan.`);
-    }
-  };
+  const [isSeen, setIsSeen] = useState(false);
+  const { addToSeenMovies, movieItems } = useContext(MovieContext);
 
   useEffect(() => {
-    localStorage.setItem("myMovies", JSON.stringify(myMovies));
-  }, [myMovies]);
+    const isAlreadyInList = movieItems.some((item) => item.id === movie.id);
+    setIsSeen(isAlreadyInList);
+  }, [movieItems, movie.id]);
 
-  return (
+  return isSeen ? null : (
     <Button
       variant="outlined"
       color="inherit"
-      onClick={markMovieAsSeen}
-      disabled={isMovieSeen}
+      onClick={() => addToSeenMovies(movie)}
       style={{
         backgroundImage: "linear-gradient(to bottom right, #FDC639, #FF7C60",
         border: 0,
         cursor: "pointer",
       }}
     >
-      {isMovieSeen ? "Seen It" : "Add"}
+      Add
     </Button>
   );
 }
